3270fc627237a410615141f58869bb01eb0d9d2b
OliverWales <oliverfwales@gmail.com>
2020-09-22 09:40:46 +0100

    Hello, SFML!

--- TheTurtleGame/TheTurtleGame/Main.cpp
+++ TheTurtleGame/TheTurtleGame/Main.cpp
@@ -1,5 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "The Turtle Game", sf::Style::Fullscreen);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+                window.close();
+        }
+
+        window.clear();
+        // draw
+        window.display();
+    }
+
+    return 0;
 }
