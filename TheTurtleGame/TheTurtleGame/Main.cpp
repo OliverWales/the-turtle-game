31a70c4c5451b6fe7aaf0f62d5a1d878098ba5ea
OliverWales <oliverfwales@gmail.com>
2020-09-22 12:15:29 +0100

    Implement split screen

--- TheTurtleGame/TheTurtleGame/Main.cpp
+++ TheTurtleGame/TheTurtleGame/Main.cpp
@@ -10,18 +10,23 @@ double inline lerp(double v0, double v1, double t) {
 int main()
 {
     sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "The Turtle Game", sf::Style::Fullscreen);
-    int screenWidth = window.getSize().x / 4;
+    int screenWidth = window.getSize().x / 8;
     int screenHeight = window.getSize().y / 4;
-
-    sf::View view;
-    view.setSize(sf::Vector2f(screenWidth, screenHeight));
-    view.setCenter(sf::Vector2f(screenHeight / 2.0, screenHeight / 2.0));
-    window.setView(view);
 
     sf::Clock clock;
 
     Turtle turtle1 = Turtle("TestTurtle.png", sf::Vector2f(0, 0), sf::Keyboard::W, sf::Keyboard::S, sf::Keyboard::A, sf::Keyboard::D);
     Turtle turtle2 = Turtle("TestTurtle.png", sf::Vector2f(50, 0), sf::Keyboard::Up, sf::Keyboard::Down, sf::Keyboard::Left, sf::Keyboard::Right);
+
+    sf::View player1View;
+    player1View.setSize(sf::Vector2f(screenWidth, screenHeight));
+    player1View.setCenter(turtle1.Position);
+    player1View.setViewport(sf::FloatRect(0.f, 0.f, 0.5f, 1.f));
+
+    sf::View player2View;
+    player2View.setSize(sf::Vector2f(screenWidth, screenHeight));
+    player2View.setCenter(turtle2.Position);
+    player2View.setViewport(sf::FloatRect(0.5f, 0.f, 0.5f, 1.f));
 
     while (window.isOpen())
     {
@@ -37,15 +42,24 @@ int main()
         double elapsedTime = clock.restart().asMilliseconds();
         turtle1.tryMove(elapsedTime);
         turtle2.tryMove(elapsedTime);
-
-        double viewX = lerp(view.getCenter().x, turtle1.Position.x, 0.003 * elapsedTime);
-        double viewY = lerp(view.getCenter().y, turtle1.Position.y, 0.003 * elapsedTime);
-        view.setCenter(sf::Vector2f(viewX, viewY));
-        window.setView(view);
-
         window.clear(sf::Color(80, 120, 180));
+
+        double player1ViewX = lerp(player1View.getCenter().x, turtle1.Position.x, 0.003 * elapsedTime);
+        double player1ViewY = lerp(player1View.getCenter().y, turtle1.Position.y, 0.003 * elapsedTime);
+        player1View.setCenter(sf::Vector2f(player1ViewX, player1ViewY));
+
+        window.setView(player1View);
         window.draw(turtle1);
         window.draw(turtle2);
+
+        double player2ViewX = lerp(player2View.getCenter().x, turtle2.Position.x, 0.003 * elapsedTime);
+        double player2ViewY = lerp(player2View.getCenter().y, turtle2.Position.y, 0.003 * elapsedTime);
+        player2View.setCenter(sf::Vector2f(player2ViewX, player2ViewY));
+
+        window.setView(player2View);
+        window.draw(turtle1);
+        window.draw(turtle2);
+
         window.display();
     }
 
